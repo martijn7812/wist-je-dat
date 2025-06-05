bc12e537301e948ae23fe59581c9f75c6e08bf01
Martijn Broekman <m.broekman@opspoor.nl>
2025-06-05 15:07:34 +0200

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WistJeDatApp from "./WistJeDatApp";
+import wistjedatapp from "./WistJeDatApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
