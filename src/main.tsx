02921bf6ed275d1db49d3219bba6c5fd79627476
Martijn Broekman <m.broekman@opspoor.nl>
2025-06-05 16:52:33 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import WistJeDatApp from "./wjdapp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
