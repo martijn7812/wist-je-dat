c160e9cda96439d42a8fda5a4db1cbcf07cdeeca
martijn7812 <155836586+martijn7812@users.noreply.github.com>
2025-06-05 14:28:19 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WistJeDatApp from "./WistJeDatApp";
+import WistJeDatApp from "./wistjedatapp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
