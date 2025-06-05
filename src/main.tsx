af29dbbb902defc894b4b26a21730752a1dd11e6
Martijn Broekman <m.broekman@opspoor.nl>
2025-06-05 16:46:27 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import wjdapp from "./wjdapp";
+import WistJeDatApp from "./wjdapp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <wjdapp />
+    <WistJeDatApp />
   </React.StrictMode>
 );
